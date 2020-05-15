e6afffa8b53d04c9cd8fb67349599674d6a24c03
장민종 <alswhddl10@naver.com>
2020-05-15 09:35:16 +0900

    ..

---
+++ SwiftUI_Animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Animations
+//
+//  Created by 장민종 on 2020/05/15.
+//  Copyright © 2020 장민종. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
